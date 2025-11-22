35e2a0fd5c4ee17941fd69e422c40d470c5c7078
yu-rim98 <rimmm098@gmail.com>
2025-11-22 14:40:19 +0900

    Chore: 프로젝트 생성

---
+++ src/main/java/com/sayye/SayyeApplication.java
@@ -0,0 +1,13 @@
+package com.sayye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayye/SayyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
