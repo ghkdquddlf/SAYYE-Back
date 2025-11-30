920753783b62d94c8daa729c32dffdfe947b1f6c
yu-rim98 <rimmm098@gmail.com>
2025-11-30 15:34:26 +0900

    Feat: Repository 인터페이스 추가

---
+++ src/main/java/com/sayye/reservation/repository/ReservationRepository.java
@@ -0,0 +1,10 @@
+package com.sayye.reservation.repository;
+
+import com.sayye.reservation.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+}
