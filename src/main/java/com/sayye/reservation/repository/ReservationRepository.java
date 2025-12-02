2e57891d699d82d0adecef521688f16ecc1002c7
yu-rim98 <rimmm098@gmail.com>
2025-12-02 16:11:11 +0900

    Feat: 예약 전체 조회 기능 구현

--- src/main/java/com/sayye/reservation/repository/ReservationRepository.java
+++ src/main/java/com/sayye/reservation/repository/ReservationRepository.java
@@ -1,10 +1,18 @@
 package com.sayye.reservation.repository;
 
 import com.sayye.reservation.entity.Reservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    // Todo 데이터가 많을 때 응답 속도 비교해보기
+    @Override
+    @EntityGraph(attributePaths = {"room", "course"})
+    Page<Reservation> findAll(Pageable pageable);
 
 }
