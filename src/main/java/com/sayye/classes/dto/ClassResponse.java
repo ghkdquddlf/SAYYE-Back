0604aa5d63060d13b141c23cc8657558d68f288a
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-12-07 22:54:09 +0900

    ClassResponse(응답) 생성

---
+++ src/main/java/com/sayye/classes/dto/ClassResponse.java
@@ -0,0 +1,28 @@
+package com.sayye.classes.dto;
+
+import com.sayye.classes.entity.Class;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+// 클래스 응답 DTO
+@Getter
+@Builder
+public class ClassResponse {
+
+    private Long id;
+    private String name;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    // Entity -> Response DTO 변환
+    public static ClassResponse from(Class classEntity) {
+        return ClassResponse.builder()
+            .id(classEntity.getId())
+            .name(classEntity.getName())
+            .startDate(classEntity.getStartDate())
+            .endDate(classEntity.getEndDate())
+            .build();
+    }
+}
