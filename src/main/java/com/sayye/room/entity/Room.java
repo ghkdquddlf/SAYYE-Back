877840a51b50befbb4a32b9bf5d0d4ea7a279809
ghkdquddlf <xhdlapdlxm12@naver.com>
2025-12-07 13:46:57 +0900

    Room Entity roomName필드 unique 지정

--- src/main/java/com/sayye/room/entity/Room.java
+++ src/main/java/com/sayye/room/entity/Room.java
@@ -28,7 +28,7 @@ public class Room {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String roomName;
 
     @Column(nullable = false)
