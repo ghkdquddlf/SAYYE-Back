f4b449ea9ddad12f610454611e5fedf0c1a3b040
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-12-07 22:38:14 +0900

    class 엔티티

---
+++ src/main/java/com/sayye/classes/entity/Class.java
@@ -0,0 +1,44 @@
+package com.sayye.classes.entity;
+
+import com.sayye.baseEntity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Table(name = "classes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Class extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name; // 클래스 이름
+
+    @Column(nullable = false)
+    private LocalDate startDate; // 시작일
+
+    @Column(nullable = false)
+    private LocalDate endDate; // 종료일
+
+    @Builder
+    public Class(String name, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    // 클래스 정보 수정 메서드
+    public void update(String name, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
