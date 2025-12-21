ec0f98fe6c62a2c860ee38231b8b86a69b221218
yu-rim98 <rimmm098@gmail.com>
2025-12-21 13:32:07 +0900

    Refactor: 관리자 예약 시 enum 필드명 수정

--- src/main/java/com/sayye/reservation/entity/ReservationStatus.java
+++ src/main/java/com/sayye/reservation/entity/ReservationStatus.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum ReservationStatus {
     RESERVED("예약"),
     CANCELED("취소"), // 취소됨
-    UNAVAILABLE("예약 불가"), // 예약 불가
+    ADMIN_RESERVED("관리자 예약"),
     CANCELLED_BY_ADMIN("관리자에 의한 취소"),
     FINISHED("완료");  // 사용 완료
 
