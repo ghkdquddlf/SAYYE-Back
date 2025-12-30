13fb8f448e62ccf743ff859b7b267bf21221412e
ghkdquddlf <xhdlapdlxm12@naver.com>
2025-12-30 16:30:39 +0900

    Refactor : 사용하는 부분이 중복됨에 따라 DTO 이름에서 Create 제거

--- src/main/java/com/sayye/notice/dto/request/NoticeReqDto.java
+++ src/main/java/com/sayye/notice/dto/request/NoticeReqDto.java
@@ -3,11 +3,10 @@ package com.sayye.notice.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-public class CreateNoticeReqDto {
+public class NoticeReqDto {
 
     @NotBlank
     private String title;
