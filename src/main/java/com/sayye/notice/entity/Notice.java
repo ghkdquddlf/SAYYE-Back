64be9592e0be4e722ef68a8d7763e46b0776c83e
ghkdquddlf <xhdlapdlxm12@naver.com>
2025-12-30 10:36:36 +0900

    Feat : Notice Entity 설정

--- src/main/java/com/sayye/notice/entity/Notice.java
+++ src/main/java/com/sayye/notice/entity/Notice.java
@@ -1,8 +1,37 @@
 package com.sayye.notice.entity;
 
+import com.sayye.admin.entity.Admin;
+import com.sayye.baseEntity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
 @Getter
-public class Notice {
+@Entity
+@Table(name = "notice")
+public class Notice extends BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private boolean status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "admins_id", nullable = false)
+    private Admin admin;
 }
