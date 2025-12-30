1a9738848bfccdddfb1fd0bb83a145830ceee338
ghkdquddlf <xhdlapdlxm12@naver.com>
2025-12-30 15:37:12 +0900

    Feat : noticesRes/Req 필드 정의

--- src/main/java/com/sayye/notice/dto/request/CreateNoticeReqDto.java
+++ src/main/java/com/sayye/notice/dto/request/CreateNoticeReqDto.java
@@ -1,8 +1,17 @@
 package com.sayye.notice.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 public class CreateNoticeReqDto {
 
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
 }
