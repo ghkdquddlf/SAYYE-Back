88324e016d2d4b471456d1de21941342fb578e26
sshkim1013 <sshkim1013@naver.com>
2025-12-30 11:10:38 +0900

    관리자 추가 시 입력 값 검증 코드 작성

--- src/main/java/com/sayye/admin/dto/request/SignupRequest.java
+++ src/main/java/com/sayye/admin/dto/request/SignupRequest.java
@@ -2,6 +2,8 @@ package com.sayye.admin.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -10,12 +12,18 @@ import lombok.RequiredArgsConstructor;
 public class SignupRequest {
 
     @NotBlank(message = "아이디는 필수입니다.")
+    @Size(min = 4, max = 20, message = "아이디는 4자 이상 20자 이하로 입력해주세요.")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "아이디는 영문 소문자와 숫자만 사용 가능합니다.")
     private final String userId;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&]{8,20}$",
+             message = "비밀번호는 8자 이상 20자 이하의 영문, 숫자를 포함해야 합니다."
+    )
     private final String password;
 
     @NotBlank(message = "이름은 필수입니다.")
+    @Size(min = 2, max = 10, message = "이름은 2자 이상 10자 이하로 입력해주세요.")
     private final String name;
 
     @NotBlank(message = "이메일은 필수입니다.")
