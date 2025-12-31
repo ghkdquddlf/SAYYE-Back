1b25599658eaefd960200b3bfe24c94c452bf92d
sshkim1013 <sshkim1013@naver.com>
2025-12-31 09:03:46 +0900

    관리자 추가 시, 아이디와 비밀번호 검증 코드 수정.

--- src/main/java/com/sayye/admin/dto/request/SignupRequest.java
+++ src/main/java/com/sayye/admin/dto/request/SignupRequest.java
@@ -2,7 +2,6 @@ package com.sayye.admin.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,14 +11,11 @@ import lombok.RequiredArgsConstructor;
 public class SignupRequest {
 
     @NotBlank(message = "아이디는 필수입니다.")
-    @Size(min = 4, max = 20, message = "아이디는 4자 이상 20자 이하로 입력해주세요.")
-    @Pattern(regexp = "^[a-z0-9]+$", message = "아이디는 영문 소문자와 숫자만 사용 가능합니다.")
+    @Size(min = 4, message = "아이디는 4자 이상이어야 합니다.")
     private final String userId;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&]{8,20}$",
-             message = "비밀번호는 8자 이상 20자 이하의 영문, 숫자를 포함해야 합니다."
-    )
+    @Size(min = 4, message = "비밀번호는 4자 이상이어야 합니다.")
     private final String password;
 
     @NotBlank(message = "이름은 필수입니다.")
